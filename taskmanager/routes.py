a7c60c5805f3aa098fc295567372fc08da6851ae
Mark Dunne <Markdunne117@gmail.com>
2023-10-20 18:54:35 +0000

    Add functionality to update categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -24,3 +24,17 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    # get_or_404 is a sqlalchemy method that attempts to find the specified
+    # record. if no record is found, it returns a 404 error.
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    # 1st category is the variable used in the edit category template
+    # 2nd category is the variable defined above
+    return render_template("edit_category.html", category=category)
