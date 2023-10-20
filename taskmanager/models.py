c109ce70730d639f0e9797157eb3bb0910a9073a
Mark Dunne <Markdunne117@gmail.com>
2023-10-20 15:15:17 +0000

    Create taskmanager database

---
+++ taskmanager/models.py
@@ -0,0 +1,34 @@
+from taskmanager import db
+
+
+class Category(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    # linking our foreign key and cascade deletion
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        # similar to the this keyword in javascript
+        return self.category_name
+
+
+class Task(db.Model):
+    # schema for the Task model
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    # foreign key value points to id of category class
+    # ondelete="CASCADE" used for a one-to-many relationship
+    # this means that One category can be applied to many different
+    # tasks, but one task cannot have many categories.
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
